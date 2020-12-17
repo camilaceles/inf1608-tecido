21470a8385100c07d2c768c31ddc9c46b6ac7f42
Camila Celes <camilaceles@gmail.com>
2020-12-17 14:39:53 -0300

    working

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ straight_bar_size = 1
 diagonal_bar_size = math.sqrt(2)
 
 mass = 0.007 # mass of each point
-wind_force = [-2, 2, -5]
+wind = [10, 0, 2]
 gravity = [0, -9.8, 0]
 
 points = [] # [x, y, z]
@@ -23,9 +23,9 @@ bars = [] # [vertice_index, vertice_index, size]
 secondary_bars = [] # [vertice_index, vertice_index, size]
 
 camera_pos = [
-  (10, 25, 120), # camera location
-  (20, 0, 0),    # focus point
-  (0, 0, 0),     # viewup vector
+  (20, 0, 120), # camera location
+  (20, 0, 0),   # focus point
+  (0, 0, 0),    # viewup vector
 ]
 
 #-------------------------------------------------------------------
@@ -33,25 +33,60 @@ camera_pos = [
 #-------------------------------------------------------------------
 def is_mobile(index):
   """Returns wether the point with given index is mobile or not"""
-  return index >= size_y
+  return index%size_y != 0
+  # return index != 0 and index != size_y-1
+
+def norm(v):
+  """Returns norm of vector v"""
+  sum_ = 0
+  for val in v:
+    sum_ += val*val
+  return math.sqrt(sum_)
+
+def add(u, v):
+  """Returns (u+v)"""
+  w = u.copy()
+  for i,val in enumerate(v):
+    w[i] += val
+  return w
+
+def subtract(u, v):
+  """Returns (u-v)"""
+  w = u.copy()
+  for i,val in enumerate(v):
+    w[i] -= val
+  return w
+
+def multiply(v, k):
+  """Returns multiplication of vector v with scalar k"""
+  u = v.copy()
+  for i,val in enumerate(u):
+    u[i] *= k
+  return u
 
 #-------------------------------------------------------------------
 # Methods
 #-------------------------------------------------------------------
 def impose_constraint():
   """Imposes constraint on bars"""
-  for k in range(0, 20): # TODO how many times?
+  for k in range(0, 20): # 50
     for i,bar in enumerate(bars+secondary_bars):
       a = points[bar[0]]
       b = points[bar[1]]
-      d = np.subtract(b, a)
-      dist = np.linalg.norm(d)
-      u = np.multiply(d, 1/dist)
-      dif = 0.9*(dist - bar[2])/2
+      d = subtract(b, a)
+      dist = norm(d)
+      u = multiply(d, 1/dist)
+      dif = 0.9*(dist - bar[2])
       if is_mobile(bar[0]):
-        points[bar[0]] = np.add(a, np.multiply(u, dif))
+        if is_mobile(bar[1]):
+          points[bar[0]] = add(a, multiply(u, dif/2))
+        else:
+          points[bar[0]] = add(a, multiply(u, dif))
       if is_mobile(bar[1]):
-        points[bar[1]] = np.add(b, np.multiply(u, -dif))
+        if is_mobile(bar[0]):
+          points[bar[1]] = add(b, multiply(u, -dif/2))
+        else:
+          points[bar[1]] = add(b, multiply(u, -dif))
 
 def animate(h):
   """Animates mesh"""
@@ -62,16 +97,19 @@ def animate(h):
   # First, we move each point independently
   for i,point in enumerate(points):
     if is_mobile(i):
-      point[0] = point[0] + ((1 - damp_ratio)*(point[0] - last_points[i][0])) + (h*h/mass)*wind_force[0]
-      point[1] = point[1] + ((1 - damp_ratio)*(point[1] - last_points[i][1])) + (h*h/mass)*gravity[1]
-      point[2] = point[2] + ((1 - damp_ratio)*(point[2] - last_points[i][2])) + (h*h/mass)*wind_force[2]
-
+      point[0] = point[0] + ((1 - damp_ratio)*(point[0] - last_points[i][0]))\
+        + (h*h/mass)*(wind[0]+gravity[0])
+      point[1] = point[1] + ((1 - damp_ratio)*(point[1] - last_points[i][1]))\
+        + (h*h/mass)*(wind[1]+gravity[1])
+      point[2] = point[2] + ((1 - damp_ratio)*(point[2] - last_points[i][2]))\
+        + (h*h/mass)*(wind[2]+gravity[2])
   # Then, we impose constraint bars restrictions
   impose_constraint()
   last_points = points_i
 
 def init():
   """Sets up points and faces in mesh"""
+  global last_points
   point_index = 0
   for j in range(0, size_x):
     for i in range(0, size_y):
@@ -98,7 +136,6 @@ def init():
 
       point_index += 1
 
-  global last_points
   last_points = points.copy()
 
 #-------------------------------------------------------------------
@@ -110,17 +147,18 @@ print("To star the animation, click 'q'")
 # Plotting mesh
 plotter = pv.Plotter()
 mesh = pv.PolyData(np.array(points), np.array(faces))
-plotter.add_mesh(mesh, color='w', show_edges=True, interpolate_before_map=True)
+plotter.add_mesh(mesh, color='y', show_edges=False, interpolate_before_map=True)
 plotter.add_axes()
 plotter.enable_eye_dome_lighting()
 plotter.camera_position = camera_pos
 plotter.show(interactive=True, auto_close=False, window_size=[800, 600])
 
-
 plotter.open_gif("animation.gif")
 
-for i in range(0, 70):
+n_steps = 75
+for i in range(n_steps):
   animate(0.033)
+  print(f"   Plotting step {i+1} of {n_steps}...", end="\r")
   plotter.update_coordinates(np.array(points), mesh=mesh)
   plotter.write_frame()
 
